b0481e4d86f56b2ec66c65b3419db98396dee4bf
Sunil Sharma <sunilsharma.workit@gmail.com>
2023-05-29 12:41:45 +0530

    <>

--- C#/Code/Assessment_3.cs
+++ C#/Code/Assessment_3.cs
@@ -11,16 +11,17 @@
             inputNumber = Convert.ToInt32(Console.ReadLine());
 
             //defing the logic
-            for (int i = 1; i <= inputNumber; i++)
+            for (int i = 2; i <= Math.Sqrt(inputNumber); i++)
             {
                 if(inputNumber % i == 0)
                 {
                     count++;
+                    break;
                 }
             }
 
             //checking if the count value is 2 or more than  2
-            if (count == 2)
+            if (count == 0)
             {
                 Console.WriteLine("{0} is Prime number", inputNumber);
             }
