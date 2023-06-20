ad0887f5cd8d21d48979ab8afb4324ac5b642645
Sunil Sharma <sunilsharma.workit@gmail.com>
2023-06-20 19:01:24 +0530

    <>

---
+++ C#/LINQ  Assignment/Assignment 3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment_3
+{
+    class FindTheString
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter list of strings (sperated with coma's)");
+            string inputOfString = Console.ReadLine();
+
+            string[] collectionOfString = inputOfString.Split(',');
+
+            //linq query for finding the shortest string
+            var shortestString = from i in collectionOfString
+                                 orderby i.Length
+                                 select i;
+
+            //linq query for finding the longest string
+            var longestString = from i in collectionOfString
+                                orderby i.Length descending
+                                select i;
+
+            Console.WriteLine("-----\nShortest string is: {0}", shortestString.FirstOrDefault());
+            Console.WriteLine("Longest string is : {0}", longestString.FirstOrDefault());
+        }
+    }
+}
