90ad69b08304dee3014e7e3f4eb5ca7e97e24ff4
Sunil Sharma <sunilsharma.workit@gmail.com>
2023-06-21 18:23:50 +0530

    <>

---
+++ C#/LINQ  Assignment/Assignment 9/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Assignment_9
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter List of string (separate with coma)");
+            string listOfString = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(listOfString))
+            {
+                Console.WriteLine("Input cannot be empty or null");
+                return;
+            }
+
+            else { }
+            string[] arrayOfString = listOfString.Split(',');
+
+            //linq query
+            var stringStartWithA = from i in arrayOfString
+                                   where i.StartsWith("A")
+                                   select i;
+
+            Console.WriteLine("--------------\nList of string that starts with character 'A' ");
+            foreach ( var i in stringStartWithA )
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
