09971787dc38fea299e14405f0de6603a1583529
Sunil Sharma <sunilsharma.workit@gmail.com>
2024-07-29 20:05:16 +0530

    document object model (DOM)

---
+++ Web Development/Java Script/07_dom/cssSelectors.js
@@ -0,0 +1,6 @@
+function colorChange() {
+    let heading = document.querySelector('h1');
+    heading.innerText = "xRt";
+}
+
+setTimeout(colorChange, 3000);
---
+++ Web Development/Java Script/07_dom/selectingDotSyntax.js
@@ -0,0 +1,11 @@
+let bodyElement = document.body;
+console.log(bodyElement);
+
+let imageElements = document.images;
+console.log(imageElements);
+
+let linkElements = document.links;
+console.log(linkElements);
+
+let formElements = document.forms;
+console.log(formElements);
