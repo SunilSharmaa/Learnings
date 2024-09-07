f1334279180cbe8be00e8dcabc65bb5a60038059
Sunil Sharma <sunilsharma.workit@gmail.com>
2024-09-07 16:44:09 +0530

    sorting practice

---
+++ Web Development/Java Script/practice/sort.js
@@ -0,0 +1,17 @@
+let arr = [6,23,74,1,12,];
+
+console.log(arr);
+
+console.log("sorted array");
+
+for(let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < arr.length - i; j++) {
+        if(arr[j] > arr[j + 1]) {
+            let temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+    }
+}
+
+console.log(arr);
