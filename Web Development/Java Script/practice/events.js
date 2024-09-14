e47cb9da418e2db7ffccb490f2d8489a81eee615
Sunil Sharma <sunilsharma.workit@gmail.com>
2024-09-14 13:25:07 +0530

    events

--- Web Development/Java Script/practice/events.js
+++ Web Development/Java Script/practice/events.js
@@ -1,1 +1,18 @@
 //event bubbling
+let grandParent = document.getElementById("grand-parent");
+grandParent.addEventListener("click", (e)=> {
+    console.log("Grand Parent");
+    // e.stopPropagation();
+},true)
+
+let parent = document.getElementById("parent");
+parent.addEventListener("click", (e)=> {
+    console.log("Parent");
+    // e.stopPropagation();
+},true)
+
+let me = document.getElementById("me");
+me.addEventListener("click", (e)=> {
+    console.log("Me");
+    // e.stopPropagation();
+},true)
