8e2e772d49da95798b6e6f62abfb6f8b9ef54bf1
Sunil Sharma <sunilsharma.workit@gmail.com>
2024-12-10 20:39:26 +0530

    exploring jsx syntax

--- Web Development/React/chapter3_laying_the_foundation/main.js
+++ Web Development/React/chapter3_laying_the_foundation/main.js
@@ -1,11 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-let div = (<div>
+let Footer = () => <h4>Footer</h4>;
+let Heading = () => {
+  return <h2>Heading</h2>;
+};
+
+
+let div = (
+  <div>
+    <Heading />
     <h1>imported jsx_</h1>
     <div>
-        <h2>export jsx_</h2>
+      <h2>export jsx_</h2>
     </div>
-</div>);
+    {<Footer />}
+  </div>
+);
+
 let root = ReactDOM.createRoot(document.getElementById("head"));
-root.render(div)
+root.render(div);
