af228cce497f85eb54a9e494ce9d7eb0a7902919
Sunil Sharma <sunilsharma.workit@gmail.com>
2024-12-11 12:27:36 +0530

    composition component

--- Web Development/React/chapter3_laying_the_foundation/assignment2.js
+++ Web Development/React/chapter3_laying_the_foundation/assignment2.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+let Para = ()=> {
+    return <p>paragraph</p>;
+}
 let Heading = ()=> {
     return (<div class="title">
         <h1>heading 1</h1>
+        {<Para /> }
         <h2>heading 2</h2>
+        {<Para />}
         <h3>heading 3</h3>
+        {<Para />}
     </div>)
 }
 
