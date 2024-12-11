b49071429c87256d9274c90a5b477e870c486c97
Sunil Sharma <sunilsharma.workit@gmail.com>
2024-12-11 11:26:11 +0530

    react assignment 3

---
+++ Web Development/React/chapter3_laying_the_foundation/assignment2.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+let Heading = ()=> {
+    return (<div class="title">
+        <h1>heading 1</h1>
+        <h2>heading 2</h2>
+        <h3>heading 3</h3>
+    </div>)
+}
+
+let root = ReactDOM.createRoot(document.getElementById("container"));
+root.render(<Heading />)
